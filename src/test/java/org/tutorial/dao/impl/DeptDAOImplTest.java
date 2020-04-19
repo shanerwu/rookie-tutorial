deea875fb59597273bad44c63ef36cb727fb084a
shanerwu <yunwu0128@gmail.com>
2020-04-19 23:23:20 +0800

    test: Add unit test

---
+++ src/test/java/org/tutorial/dao/impl/DeptDAOImplTest.java
@@ -0,0 +1,77 @@
+package org.tutorial.dao.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.tutorial.dao.DeptDAO;
+import org.tutorial.model.DeptVO;
+import org.tutorial.model.EmpVO;
+
+public class DeptDAOImplTest {
+
+    private DeptDAO dao;
+
+    @Before
+    public void init() {
+        dao = new DeptDAOImpl();
+    }
+
+    @Test
+    public void insert() {
+        DeptVO deptVO = new DeptVO();
+        deptVO.setDname("製造部");
+        deptVO.setLoc("美國洛杉磯");
+        dao.insert(deptVO);
+    }
+
+    @Test
+    public void update() {
+        DeptVO deptVO = new DeptVO();
+        deptVO.setDeptno(10);
+        deptVO.setDname("財務部2");
+        deptVO.setLoc("臺灣台北2");
+        dao.update(deptVO);
+    }
+
+    @Test
+    public void delete() {
+        dao.delete(30);
+    }
+
+    @Test
+    public void findByPrimaryKey() {
+        DeptVO deptVO = dao.findByPrimaryKey(10);
+		System.out.print(deptVO.getDeptno() + ",");
+		System.out.print(deptVO.getDname() + ",");
+		System.out.println(deptVO.getLoc());
+		System.out.println("---------------------");
+    }
+
+    @Test
+    public void getAll() {
+        List<DeptVO> list = dao.getAll();
+        for (DeptVO aDept : list) {
+            System.out.print(aDept.getDeptno() + ",");
+            System.out.print(aDept.getDname() + ",");
+            System.out.print(aDept.getLoc());
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void getEmpsByDeptno() {
+        Set<EmpVO> set = dao.getEmpsByDeptno(10);
+        for (EmpVO aEmp : set) {
+            System.out.print(aEmp.getEmpno() + ",");
+            System.out.print(aEmp.getEname() + ",");
+            System.out.print(aEmp.getJob() + ",");
+            System.out.print(aEmp.getHiredate() + ",");
+            System.out.print(aEmp.getSal() + ",");
+            System.out.print(aEmp.getComm() + ",");
+            System.out.print(aEmp.getDeptno());
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/org/tutorial/dao/impl/EmpDAOImplTest.java
@@ -0,0 +1,77 @@
+package org.tutorial.dao.impl;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.tutorial.dao.EmpDAO;
+import org.tutorial.model.EmpVO;
+
+public class EmpDAOImplTest {
+
+    private EmpDAO dao;
+
+    @Before
+    public void init() {
+        dao = new EmpDAOImpl();
+    }
+
+    @Test
+    public void insert() {
+        EmpVO empVO = new EmpVO();
+        empVO.setEname("王小明1");
+        empVO.setJob("manager");
+        empVO.setHiredate(java.sql.Date.valueOf("2020-04-01"));
+        empVO.setSal(new Double(50000));
+        empVO.setComm(new Double(500));
+        empVO.setDeptno(10);
+        dao.insert(empVO);
+    }
+
+    @Test
+    public void update() {
+        EmpVO empVO = new EmpVO();
+        empVO.setEmpno(7001);
+        empVO.setEname("王小明2");
+        empVO.setJob("manager2");
+        empVO.setHiredate(java.sql.Date.valueOf("2020-04-01"));
+        empVO.setSal(new Double(20000));
+        empVO.setComm(new Double(200));
+        empVO.setDeptno(20);
+        dao.update(empVO);
+    }
+
+    @Test
+    public void delete() {
+        dao.delete(7014);
+    }
+
+    @Test
+    public void findByPrimaryKey() {
+        EmpVO empVO = dao.findByPrimaryKey(7001);
+        System.out.print(empVO.getEmpno() + ",");
+        System.out.print(empVO.getEname() + ",");
+        System.out.print(empVO.getJob() + ",");
+        System.out.print(empVO.getHiredate() + ",");
+        System.out.print(empVO.getSal() + ",");
+        System.out.print(empVO.getComm() + ",");
+        System.out.println(empVO.getDeptno());
+        System.out.println("---------------------");
+    }
+
+    @Test
+    public void getAll() {
+        List<EmpVO> list = dao.getAll();
+        for (EmpVO aEmp : list) {
+            System.out.print(aEmp.getEmpno() + ",");
+            System.out.print(aEmp.getEname() + ",");
+            System.out.print(aEmp.getJob() + ",");
+            System.out.print(aEmp.getHiredate() + ",");
+            System.out.print(aEmp.getSal() + ",");
+            System.out.print(aEmp.getComm() + ",");
+            System.out.print(aEmp.getDeptno());
+            System.out.println();
+        }
+    }
+
+}
